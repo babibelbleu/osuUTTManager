f4659c1549b7f1841ba57a71d9336eead8fea5f4
Bastien Bodin <bastien.bodin.etu@univ-lemans.fr>
2023-11-29 17:22:53 +0100

    Discord bot test with command

--- main.py
+++ main.py
@@ -8,12 +8,37 @@ from Client import Client
 
 import gspread
 
+import discord
+from discord import app_commands
+from discord.ext import commands
+
 load_dotenv()
 
 CLIENT_TOKEN = os.getenv("OSU_TOKEN")
+DISCORD_BOT_TOKEN = os.getenv("BOT_TOKEN")
 ENVIRONMENT = os.getenv("ENVIRONMENT")
 
+intents = discord.Intents.default()
+intents.message_content = True
+
+bot = commands.Bot(command_prefix="!", intents=intents)
+
 client = Client(CLIENT_TOKEN)
+
+
+@bot.event
+async def on_ready():
+    print(f"Bot logged in as {bot.user}")
+    try:
+        synced = await bot.tree.sync()
+        print(f"Synced {len(synced)} commands.")
+    except Exception as e:
+        print(e)
+
+
+@bot.tree.command(name="yo")
+async def yo(interaction: discord.Interaction):
+    await interaction.response.send_message(f"Yo {interaction.user.mention}", ephemeral=True)
 
 
 def main():
@@ -79,4 +104,6 @@ def test():
 
 
 # test()
-main()
+# main()
+
+bot.run(DISCORD_BOT_TOKEN)
