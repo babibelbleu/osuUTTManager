3827417453f0b255d4220cbef7ccb3430946a394
Bastien Bodin <bastien.bodin.etu@univ-lemans.fr>
2023-11-29 16:54:43 +0100

    Refactorised code + added google sheets scores implementation

--- main.py
+++ main.py
@@ -1,13 +1,16 @@
 from pprint import pprint
 from dotenv import load_dotenv
 import os
+
+from gspread import Worksheet
+
 from Client import Client
 
 import gspread
 
 load_dotenv()
 
-CLIENT_TOKEN = os.getenv("TOKEN")
+CLIENT_TOKEN = os.getenv("OSU_TOKEN")
 ENVIRONMENT = os.getenv("ENVIRONMENT")
 
 client = Client(CLIENT_TOKEN)
@@ -18,7 +21,23 @@ def main():
     sheet = sa.open("UTT scores")
     worksheet = sheet.worksheet("Data")
 
-    games = client.get_multiplayer_info(111463775)["games"]
+    update_sheet_with_game_id(111463775, worksheet)
+
+
+def update_sheet_with_game_id(game_id: int, worksheet: Worksheet):
+    games = client.get_multiplayer_info(game_id)["games"]
+
+    # [
+    #   {
+    #       id: xxxxx    // id de la beatmap jouée
+    #       stage : LN1
+    #       player1 : 0
+    #       player2 : 0
+    #       ...
+    #   },
+    #   ...
+    # ]
+    rows: list = worksheet.get_all_records()
 
     for game in games:
         scores = game["scores"]
@@ -35,6 +54,24 @@ def main():
             user = client.get_user(score["user_id"], 3)[0]["username"]
             score_done = score["score"]
             print(f"{user} : {score_done}")
+            update_score_in_sheet(user, beatmap_id, worksheet, score_done, rows)
+
+
+def update_score_in_sheet(player: str, map_id: str, worksheet: Worksheet, score: int, rows: list) -> bool:
+
+    indexCol = 0
+    indexRow = 1
+
+    for row in rows:
+        indexRow += 1
+        if str(row["id"]).casefold() == map_id.casefold():
+            for key in row.keys():
+                indexCol += 1
+                if str(key).casefold() == player.casefold():
+                    if int(row[key]) < int(score):
+                        print("Le score réalisé est supérieur à celui de la sheet !")
+                        worksheet.update_cell(indexRow, indexCol, score)
+                    return True
 
 
 def test():
